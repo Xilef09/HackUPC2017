cb7fe1f4078bd27fb4c2e208e83b16b6cf9df8a8
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2017-03-04 03:15:19 +0100

    ui-routes added and log in goes to sing up

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
 'use strict';
-angular.module('baseModel', ['ngCookies', 'ngResource', 'ui.router','ui.bootstrap']);
+angular.module('myApp', ['ui.router'])
+.config(config);
 
+function config($stateProvider, $urlRouterProvider) {
+    //['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+
+
+    $stateProvider
+        .state('login', {
+            url : '/login',
+            templateUrl : 'index.html',
+            controller  : 'LogIn'
+        })
+        .state('singUp', {
+            url : '/singUp',
+            templateUrl : "SingUp.html"
+        });
+
+};
