ee4fd4c017f799c16a770320da11515ef0f2a41d
Julian Suarez <julian4suarez@gmail.com>
2017-03-05 00:24:38 +0100

    init ndv3

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('myApp', ['ui.router', 'ngMaterial'])
+angular.module('myApp', ['ui.router', 'ngMaterial', 'nvd3'])
     .constant('MY_CONSTANTS', {
         "SERVER_IP" : "http://45.55.176.54:8080/api",
         "LOCALHOST_IP" : 'http://localhost:8080/api'
