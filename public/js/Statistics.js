98b15b949b6293cbfbc81d97f0dcb0a4b03c2df7
Julian Suarez <julian4suarez@gmail.com>
2017-03-05 05:18:30 +0100

    all proyect plot

--- public/js/Statistics.js
+++ public/js/Statistics.js
@@ -1,27 +1,40 @@
 var app = angular.module('myApp')
-    .controller('StatisticsCtrl', ['$scope', 'restService',function($scope, restService) {
+    .controller('StatisticsCtrl', ['$scope', 'restService', function($scope, restService) {
 
-        $(document).ready(function(){
-            var response = restService.get("/project", {});
-            console.log("AQUI");
-            console.log(response);
-            console.log(response['$$state']);
-            response['$$state']['value'].forEach( function (elem){
-                console.log(elem);
-            });
-            var data = [];
+        $scope.listaProj = [];
+        var data = [];
+        //comprobar si los datos son correctos
+        restService.get("/project").then(function (result) {
+            //console.log(result);
+            if(result == undefined){
+                var alert = dialog.alert({
+                    title: 'Attention',
+                    textContent: 'This is an example of how easy dialogs can be!',
+                    ok: 'Close'
+                });
 
-            /*response.forEach(function (entry){
-                data.push({x : entry.projectName , y : entry.time});
-            });
-
-            $scope.data = [
-                {
-                    key: "Cumulative Return",
-                    values: data
-                }];*/
-
+                dialog
+                    .show( alert )
+                    .finally(function() {
+                        alert = undefined;
+                    });
+                //$mdDialog.show("Bad credentials, please stop write with your trunks");
+            }
+            else{
+                for(var i=0;i<result.length;++i){
+                    //console.log(result[i].projectName);
+                    $scope.listaProj.push(result[i].projectName);
+                    data.push({x : result[i].projectName, y : result[i].time});
+                }
+                //$scope.listaProj = result;
+            }
         });
+
+        $scope.data = [
+            {
+                key: "Cumulative Return",
+                values: data
+            }];
 
         $scope.optionsBar = {
             chart: {
@@ -43,6 +56,18 @@ var app = angular.module('myApp')
                     axisLabelDistance: -10
                 }
             }
+        };
+
+        $scope.select = function(proyecto){
+            console.log(proyecto);
+            // Coger id del board
+            //if (proyecto == 'Trello 1') $scope.tareas = taskProj1;
+            //else if (proyecto == 'Trello 2') $scope.tareas = taskProj2;
+            //else if (proyecto == 'Jira1') $scope.tareas = taskProj3;
+
+            // Coger las tareas del board que estan asignadas a mi y añadirlas a tareas
+
+            $scope.selected = proyecto;
         };
 
     }]);
